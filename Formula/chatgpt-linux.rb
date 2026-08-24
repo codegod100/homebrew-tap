186e5871c217df7259178bdbd3f4246a851fb415
nandi <veronika.m.winters@gmail.com>
2026-08-24 01:16:49 -0700

    Use wrapper for ChatGPT launcher

--- Formula/chatgpt-linux.rb
+++ Formula/chatgpt-linux.rb
@@ -16,7 +16,10 @@ class ChatgptLinux < Formula
     system "tar", "xf", Dir["data.tar.*"].fetch(0)
 
     libexec.install Dir["usr/lib/chatgpt/*"]
-    bin.install_symlink libexec/"ChatGPT" => "chatgpt"
+    (bin/"chatgpt").write <<~SH
+      #!/bin/bash
+      exec "#{libexec}/ChatGPT" "$@"
+    SH
     (share/"applications").install "usr/share/applications/chatgpt.desktop"
     (share/"pixmaps").install "usr/share/pixmaps/chatgpt.png"
   end
